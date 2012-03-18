a36766bd2332215529a8dbc301705d48a56c05f7
Gwildor Sok <gwildorsok@gmail.com>
2012-03-18 22:32:57 +0100

    Fixed bug with new new tiles overriding old ones.

--- engine/lib/map.py
+++ engine/lib/map.py
@@ -10,6 +10,7 @@ class map:
 		#print(name)
 		tile_mode = False
 		f = open(prefix+'data/maps/'+name+'.map', 'r') # open map
+		k = 0
 		
 		for line in f.readlines(): # loop through file
 			if '#' in line: # comment found
@@ -17,7 +18,6 @@ class map:
 				#print(parts)
 				line = parts[0]
 			line = line.strip()
-			x = 0
 			
 			if line != '':
 				#print(line, sep = '', end = '\n') # echo whole file
@@ -27,39 +27,38 @@ class map:
 						#print(self.general)
 						tile_mode = True
 					else:
-						#print(self.tiles[x])
+						#print(str(k), self.tiles[k])
 						
-						if 'repeat_x' in self.tiles[x] or 'repeat_y' in self.tiles[x]:
-							if 'repeat_x' in self.tiles[x]:
-								repeat_x = self.tiles[x]['repeat_x']
-								del self.tiles[x]['repeat_x'] # not needed to have those in memory anymore
+						if 'repeat_x' in self.tiles[k] or 'repeat_y' in self.tiles[k]:
+							if 'repeat_x' in self.tiles[k]:
+								repeat_x = self.tiles[k]['repeat_x']
+								del self.tiles[k]['repeat_x'] # not needed to have those in memory anymore
 							else:
 								repeat_x = 1
-							if 'repeat_y' in self.tiles[x]:
-								repeat_y = self.tiles[x]['repeat_y']
-								del self.tiles[x]['repeat_y']
+							if 'repeat_y' in self.tiles[k]:
+								repeat_y = self.tiles[k]['repeat_y']
+								del self.tiles[k]['repeat_y']
 							else:
 								repeat_y = 1
 							
-							original = self.tiles[x]
+							original = self.tiles[k]
 							#print(original)
 							for i in range(repeat_x):
 								for j in range(repeat_y):
 									if i == 0 and j == 0: # tile we already have
 										continue
-									x += 1
+									k += 1
 									new = original.copy()
 									new['pos_x'] = original['pos_x'] + (i * original['width'])
 									new['pos_y'] = original['pos_y'] + (j * original['height'])
 									self.tiles.append(new)
-									#print(str(i)+', '+str(j)+' ', self.tiles[x])
+									#print(str(i)+', '+str(j)+', '+str(k), self.tiles[k])
 						
-						x += 1
+						k += 1
 						
 					self.tiles.append({})
 					
 				else: # settings
-				
 					vals = line.split(':', 1)
 					vals[0] = vals[0].strip()
 					vals[1] = vals[1].strip()
@@ -74,13 +73,14 @@ class map:
 
 					#print(vals)
 					if tile_mode:
-						self.tiles[x][vals[0]] = vals[1]
+						self.tiles[k][vals[0]] = vals[1]
 					else:
 						self.general[vals[0]] = vals[1]
 
 
 		f.close()
 		self.tiles.pop()
+		#print(self.tiles)
 		
 	def tile_within_square(self, tile, nw, se):
 		if (tile['pos_y'] + tile['height']) >= nw['y'] and tile['pos_y'] <= se['y'] and (tile['pos_x'] + tile['height']) >= nw['x'] and tile['pos_x'] <= se['x']:
--- engine/test/map_test.py
+++ engine/test/map_test.py
@@ -23,15 +23,12 @@ ne = {'x': (x + (gameW / 2)), 'y': (y - (gameH / 2))}
 images = {}
 
 for tile in map.tiles: # loop our tiles
-	if 'pos_y' not in tile: # debugging
-		print(tile)
-	else :
-		if map.tile_within_square(tile, nw, se):
-			#print(tile)
-			if tile['src'] not in images:
-				image1 = Image.open('../../data/tiles/'+tile['src'])
-				images[tile['src']] = ImageTk.PhotoImage(image1)
-			label_image = Tkinter.Label(root, image = images[tile['src']])
-			label_image.place(x = tile['pos_x'] + gameW / 2, y = tile['pos_y'] + gameH / 2, width = tile['width'], height = tile['height'])
+	if map.tile_within_square(tile, nw, se):
+		#print(tile)
+		if tile['src'] not in images:
+			image1 = Image.open('../../data/tiles/'+tile['src'])
+			images[tile['src']] = ImageTk.PhotoImage(image1)
+		label_image = Tkinter.Label(root, image = images[tile['src']])
+		label_image.place(x = tile['pos_x'] + gameW / 2, y = tile['pos_y'] + gameH / 2, width = tile['width'], height = tile['height'])
 
 root.mainloop()
