14743f35ab09b256be0397fddabdfc7837104a26
Gwildor Sok <gwildorsok@gmail.com>
2012-03-20 01:12:26 +0100

    Ditched tkinter for pygame

--- engine/test/map_test.py
+++ engine/test/map_test.py
@@ -1,34 +1,39 @@
-import sys
+import pygame, sys, os
 sys.path.append('../lib/')
-
-import Tkinter
-import Image, ImageTk
+from pygame.locals import *
 from map import *
+
+def draw_map(map_obj, x, y, gameW = 640, gameH = 480):
+	nw = {'x': (x - (gameW / 2)), 'y': (y - (gameH / 2))}
+	sw = {'x': (x - (gameW / 2)), 'y': (y + (gameH / 2))}
+	se = {'x': (x + (gameW / 2)), 'y': (y + (gameH / 2))}
+	ne = {'x': (x + (gameW / 2)), 'y': (y - (gameH / 2))}
+
+	screen.fill(000)
+	for tile in map_obj.tiles: # loop our tiles
+		if map_obj.tile_within_square(tile, nw, se):
+			#print(tile)
+			if tile['src'] not in images:
+				images[tile['src']] = pygame.image.load('../../data/tiles/'+tile['src'])
+			screen.blit(images[tile['src']], (tile['pos_x'] + x + gameW / 2, tile['pos_y'] + y + gameH / 2))
+	pygame.display.flip()
 
 map = map('main', '../../')
 
-root = Tkinter.Tk()
-root.geometry('640x480')
-root.title('Map view')
-
-x = 0
-y = 0
-gameW = 640
-gameH = 480
-nw = {'x': (x - (gameW / 2)), 'y': (y - (gameH / 2))}
-sw = {'x': (x - (gameW / 2)), 'y': (y + (gameH / 2))}
-se = {'x': (x + (gameW / 2)), 'y': (y + (gameH / 2))}
-ne = {'x': (x + (gameW / 2)), 'y': (y - (gameH / 2))}
+pygame.init()
+window = pygame.display.set_mode((640, 480))
+pygame.display.set_caption('Map view') 
+screen = pygame.display.get_surface()  
 
 images = {}
 
-for tile in map.tiles: # loop our tiles
-	if map.tile_within_square(tile, nw, se):
-		#print(tile)
-		if tile['src'] not in images:
-			image1 = Image.open('../../data/tiles/'+tile['src'])
-			images[tile['src']] = ImageTk.PhotoImage(image1)
-		label_image = Tkinter.Label(root, image = images[tile['src']])
-		label_image.place(x = tile['pos_x'] + gameW / 2, y = tile['pos_y'] + gameH / 2, width = tile['width'], height = tile['height'])
+x = 0
 
-root.mainloop()
+while True:
+	for event in pygame.event.get():
+		if event.type == QUIT:
+			sys.exit(0)
+		else:
+			print(event)
+			draw_map(map, x, 0)
+			x += 1
