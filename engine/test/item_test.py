8dc97d6cabc48cc2d8f4b5869cd4aa0ed918545a
Gwildor Sok <gwildorsok@gmail.com>
2012-10-23 21:15:21 +0200

    Added very very basic inventory for testing.

--- engine/test/item_test.py
+++ engine/test/item_test.py
@@ -19,20 +19,28 @@ up = False
 down = False
 left = False
 right = False
+inventory = {'enabled': False, 'category': 1, 'cursor': 1}
 no_key_yet = True
 gameW = 640
 gameH = 480
 font = pygame.font.Font(None, 20)
 keys = []
 notifications = []
+old_items_time = False
 
 #notifications.append({'type': 'text', 'text': 'Test!'}) # test notification
 
 while True:
-	
+
 	if no_key_yet:
 		screen.blit(font.render('Press any key to continue', True, (255, 255, 255)), (50, 50))
-		
+		screen.blit(font.render('Controls:', True, (255, 255, 255)), (50, 80)) 
+		screen.blit(font.render('- Arrow keys to move', True, (255, 255, 255)), (50, 110))
+		screen.blit(font.render('- Space or enter to interact', True, (255, 255, 255)), (50, 130))
+		screen.blit(font.render('- i to open inventory', True, (255, 255, 255)), (50, 150))
+		screen.blit(font.render('- i or esc to close inventory', True, (255, 255, 255)), (50, 170))
+		screen.blit(font.render('- numpad 4, 8, 6 and 2 to browse through inventory', True, (255, 255, 255)), (50, 190))
+
 	else:
 
 		screen.fill((0, 0, 0))
@@ -59,13 +67,13 @@ while True:
 				hero.dir = 'w'
 				dx = -2
 				dy = 0
-				
+
 		hero.hittest(dx = dx, dy = dy, items = [item])
 		hero.draw_char(screen, gameW = gameW, gameH = gameH)
-		
+
 		if not item.owner:
 			screen.blit(item.img, (item.x + gameW / 2, item.y + gameH / 2))
-			
+
 		for index, msg in enumerate(notifications):
 			if 'time_elapsed' in msg:
 				if ('remove_after' in msg and msg['time_elapsed'] > msg['remove_after']) or ('remove_after' not in msg and msg['time_elapsed'] > 5.0):
@@ -91,6 +99,17 @@ while True:
 
 			#print msg['time_elapsed']
 
+		if inventory['enabled']:
+			# Testing code. Automatically adds items
+			if not old_items_time or old_items_time != int(time.time()):
+				hero.bag.append(hero.bag[0])
+				old_items_time = int(time.time())
+			pygame.draw.rect(screen, (255, 255, 255), (10, (gameH / 3 * 2 + 10), (gameW - 20), (gameH / 3 - 20)), 2)
+			pygame.draw.rect(screen, (255, 255, 255), (10, (gameH / 3 * 2 - 20), 80, 31), 2)
+			screen.blit(font.render('Inventory', True, (255, 255, 255)), (15, (gameH / 3 * 2 - 15)))
+			for index, item in enumerate(hero.bag):
+				screen.blit(item.img, ((index % 10) * 16 + 16, ((index / 10) * 16) + (gameH / 3 * 2 + 10) + 4))
+
 		pygame.display.flip()
 		
 	for event in pygame.event.get():
@@ -106,6 +125,11 @@ while True:
 				if interaction[0]:
 					if interaction[1] == 2 and interaction[2].msg:
 						notifications.append({'type': 'pick_up_item', 'remove_after': 10.0, 'objects': [interaction[2]]})
+
+			if event.key == K_i and not inventory['enabled']:
+				inventory['enabled'] = True
+			elif (event.key == K_i or event.key == K_ESCAPE) and inventory['enabled']:
+				inventory['enabled'] = False
 			
 		elif event.type == KEYUP:
 			if event.key == 273 or event.key == 274 or event.key == 275 or event.key == 276:
