64f784d54939d2c753c4d4324d52c8b8b2eefd11
Gwildor Sok <gwildorsok@gmail.com>
2012-10-27 20:58:18 +0200

    Add item usage cap.
    This allows items which need to be charged before usage, items which
    are removed from the inventory after use, items which can only be used
    a fixed amount of times, and others. Also allows recharging of items.

--- engine/lib/item.py
+++ engine/lib/item.py
@@ -23,6 +23,10 @@ class item:
 	instant = False
 	pickup_on_touch = False
 	duration = 0
+	uses = 1
+	used = 0
+	total_used = 0
+	remove_after_used = True
 
 	def __init__(self, file, prefix=os.path.join('..', '')):
 		if file:
@@ -47,11 +51,11 @@ class item:
 					vals[0] = vals[0].strip()
 					vals[1] = vals[1].strip()
 
-					if vals[0] in ['value', 'duration']:
+					if vals[0] in ['value', 'duration', 'uses']:
 						vals[1] = int(vals[1])
 					elif vals[0] in ['burden']:
 						vals[1] = float(vals[1])
-					elif vals[0] in ['instant', 'msg', 'pickup_on_touch']:
+					elif vals[0] in ['instant', 'msg', 'pickup_on_touch', 'remove_after_used']:
 						if vals[1] == 'true':
 							vals[1] = True
 						else:
@@ -80,6 +84,10 @@ class item:
 						self.summary = vals[1]
 					if vals[0] == 'duration':
 						self.duration = vals[1]
+					if vals[0] == 'uses':
+						self.uses = vals[1]
+					if vals[0] == 'remove_after_used':
+						self.remove_after_used = vals[1]
 
 					if vals[0] == 'src':
 						self.src = vals[1]
@@ -89,8 +97,14 @@ class item:
 						self.w = self.img.get_width()
 
 	def use(self, **args):
+		if self.used < self.uses or self.uses == 0:  # maximum amount of uses hasn't been reached
+			self.used += 1
+			self.total_used += 1
 
-		target = args.get('target', self.owner)
+			target = args.get('target', self.owner)
 
-		if self.power_type == 'health':
-			target.health += self.power
+			if self.power_type == 'health':
+				target.health += self.power
+
+		if self.used >= self.uses and self.uses != 0 and self.remove_after_used:
+			self.owner.bag.remove(self)
